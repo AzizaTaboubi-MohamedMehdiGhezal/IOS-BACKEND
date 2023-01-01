68a1aebaf96c2a758893a78a0be168101b3e34c2
ghezal0n <mohamedmehdi.ghezal@esprit.tn>
2023-01-01 15:14:14 +0100

    merged aziza

--- Models/productModel.js
+++ Models/productModel.js
@@ -24,7 +24,7 @@ const productSchema = new Schema(
         },
         image: { 
            type: String,
-           required: false,
+           required: true,
              },
 
         promo: { 
